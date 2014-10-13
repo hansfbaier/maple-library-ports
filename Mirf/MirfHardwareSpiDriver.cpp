def5f51ff83e0f4e87d1406b97ad70790e7f5387
Hans Baier <hansfbaier@googlemail.com>
2014-10-13 13:38:01 +0700

    make Mirf work

--- Mirf/MirfHardwareSpiDriver.cpp
+++ Mirf/MirfHardwareSpiDriver.cpp
@@ -1,14 +1,15 @@
 #include "MirfHardwareSpiDriver.h"
 #include "HardwareSPI.h"
+#include "wirish.h"
 
-HardwareSPI spi(1);
+HardwareSPI spi(2);
 
 uint8_t MirfHardwareSpiDriver::transfer(uint8_t data){
 	return spi.transfer(data);
 }
 
 void MirfHardwareSpiDriver::begin(){
-	spi.begin(SPI_4_5MHZ, 1, 0);
+	spi.begin(SPI_4_5MHZ, MSBFIRST, SPI_MODE_0);
 }
 
 void MirfHardwareSpiDriver::end(){
